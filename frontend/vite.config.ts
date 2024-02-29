1e40b2b11789eaa2762f9451decc7ef5b11515aa
Gleb Shkut <gleb.shkut@gmail.com>
2024-02-28 18:02:28 -0700

    edit config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,36 +3,48 @@ import { defineConfig } from "vite";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    target: "es2020",
-  },
-  optimizeDeps: {
-    esbuildOptions: {
+export default defineConfig(({ command }) => {
+  const commonConfig = {
+    build: {
       target: "es2020",
     },
-  },
-  define: {
-    global: (() => {
-      let globalVariable = 'globalThis';
-      try {
-        // Try to import @safe-global/safe-apps-provider
-        require.resolve('@safe-global/safe-apps-provider');
-        // Try to import @safe-global/safe-apps-sdk
-        require.resolve('@safe-global/safe-apps-sdk');
-        // If both modules are found, return the custom global variable
-        globalVariable = 'global';
-      } catch (e) {
-        // If either module is not found, fallback to globalThis
-        globalVariable = 'globalThis';
+    optimizeDeps: {
+      esbuildOptions: {
+        target: "es2020",
+      },
+    },
+    define: {
+      global: 'globalThis',
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src/"),
       }
-      return globalVariable;
-    })()
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
+    },
+    plugins: [react()],
+  };
+  if (command === "serve") {
+    return commonConfig
+  } else {
+    return {
+      ...commonConfig,
+      define: {
+        global: (() => {
+          let globalVariable = 'globalThis';
+          try {
+            // Try to import @safe-global/safe-apps-provider
+            require.resolve('@safe-global/safe-apps-provider');
+            // Try to import @safe-global/safe-apps-sdk
+            require.resolve('@safe-global/safe-apps-sdk');
+            // If both modules are found, return the custom global variable
+            globalVariable = 'global';
+          } catch (e) {
+            // If either module is not found, fallback to globalThis
+            globalVariable = 'globalThis';
+          }
+          return globalVariable;
+        })()
+      }
     }
-  },
-  plugins: [react()],
+  }
 });
