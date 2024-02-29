20bb2636a0d6335ef6b2166f901c010ff78c4a28
Gleb Shkut <gleb.shkut@gmail.com>
2024-02-29 08:49:13 -0700

    add no wallets state

--- frontend/src/pages/MyWallets.tsx
+++ frontend/src/pages/MyWallets.tsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from "react";
 import { useAccount } from 'wagmi';
 
 export default function MyWallets() {
-  const [wallets, setWallets] = useState<Wallet[]>([])
+  const [wallets, setWallets] = useState<Wallet[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const account = useAccount()
 
   useEffect(() => {
@@ -25,9 +26,10 @@ export default function MyWallets() {
       } catch (e) {
         console.error(e);
       }
+      setIsLoading(false)
     }
 
-    if (account.address && wallets.length < 1) {
+    if (account.address && wallets.length < 1 && isLoading) {
       fetchUserNFTs();
     }
   }, [account.address])
@@ -55,9 +57,18 @@ export default function MyWallets() {
           <span className="text-center w-2/3">Michi wallets are represented as NFTs. Deposit supported tokens into these wallets to earn points.</span>
         </div>
         <div className="flex flex-col gap-5">
-          {wallets.map((wallet, index) => (
-            <WalletItem key={index} wallet={wallet} index={index + 1} />
-          ))}
+          {
+            /* {isLoading ? (
+              Array(4).fill(null).map((_, index) => (
+                <div key={index} className="skeleton w-full h-36 opacity-80"></div>
+              ))
+            ) :  */
+            wallets.length > 10 ? wallets.map((wallet, index) => (
+              <WalletItem key={index} wallet={wallet} index={index + 1} />
+            )) : <div className="mx-auto text-xl">
+              No wallets
+            </div>
+          }
         </div>
       </div>
     </div>
