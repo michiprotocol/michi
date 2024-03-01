11507b21af30c2a99ce30a085f042340d6dc9122
Gleb Shkut <gleb.shkut@gmail.com>
2024-02-29 21:50:52 -0700

    update logo, add favicon & title

--- frontend/src/shared/Logo.tsx
+++ frontend/src/shared/Logo.tsx
@@ -1,11 +1,16 @@
+import { Routes } from "@/constants/routes";
+import { Link } from "react-router-dom";
+
 export default function Logo() {
 
   return (
-    <img 
-      src="/assets/logo.jpeg"
-      alt="Logo"
-      width={50}
-      height={50}
-    />
+    <Link to={Routes.MY_WALLETS}>
+      <img
+        src="/assets/logo.png"
+        alt="Logo"
+        width={50}
+        height={50}
+      />
+    </Link>
   )
 }
