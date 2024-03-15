c0e584fe775182390a4c0b27b19c8eed5f6ce957
Gleb Shkut <gleb.shkut@gmail.com>
2024-03-15 09:36:42 -0700

    revert vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const commonConfig = {
-    base: "/app",
     build: {
       target: "es2020",
     },
